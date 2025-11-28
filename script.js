30d1508ff9860aa115f735f599c54f86bfbfffc8
psyanide <paJavier@mcm.edu.ph>
2025-11-28 19:16:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const toggleBtn = document.getElementById('theme-toggle');
-toggleBtn.addEventListener('click', () => {
-   document.body.classList.toggle('dark-mode');
-   toggleBtn.innerHTML = document.body.classList.contains('dark-mode') ? 
-       '<i class="fa-solid fa-sun"></i>' : '<i class="fa-solid fa-moon"></i>';
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
 });
